90716e4505c2d554e1e4515a8a46459c415090a2
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-27 10:20:34 +0200

    Add search function

--- secondhand-shop/frontend/src/pages/HomePage.jsx
+++ secondhand-shop/frontend/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     const [products, setProducts] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         axios
@@ -19,7 +21,18 @@ function HomePage() {
                     alt="Secondhand clothes"
                     className="cover-iamge"
                 />
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        className="search-input"
+                    />
+                    <button className="search-button"></button>
+                </div>
             </div>
+            <h2 className="section-title">See what's new</h2>
         </div>
     );
 }

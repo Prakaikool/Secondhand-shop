6227faaea9bff70250df76485907fc93c7633a38
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-29 00:52:05 +0200

    Delete fucntion for removing products

--- secondhand-shop/backend/routes/products.js
+++ secondhand-shop/backend/routes/products.js
@@ -41,4 +41,12 @@ router.get('/id', (req, res) => {
     });
 });
 
+router.deleate('/:id', (req, res) => {
+    const id = req.params.id;
+    db.run('DELETE FROM products WHERE id = ?', [id], function (err) {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json({ deleted: this.changes });
+    });
+});
+
 module.exports = router;

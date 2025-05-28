136ec9c55646a9432c23eac3c4f7a965ca629bb8
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-29 01:48:57 +0200

    Error catching

--- secondhand-shop/backend/routes/products.js
+++ secondhand-shop/backend/routes/products.js
@@ -65,4 +65,9 @@ router.put('/:id', (req, res) => {
     );
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
+});
+
 module.exports = router;

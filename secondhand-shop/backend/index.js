fc93684d14d3d03311a8772522309a06ab7ee876
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-29 02:02:19 +0200

    install multer

--- secondhand-shop/backend/index.js
+++ secondhand-shop/backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+app.use('/uploads', express.static('uploads'));
 
 const productRoutes = require('./routes/products');
 const categoryRoutes = require('./routes/categories');

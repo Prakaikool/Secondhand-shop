7739e5cd8ae8d4df326d5d4329ede5551ef9463c
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 13:33:47 +0200

    Create contact page in css and add div container

--- secondhand-shop/frontend/src/pages/ContactPage.jsx
+++ secondhand-shop/frontend/src/pages/ContactPage.jsx
@@ -1,7 +1,13 @@
 function ContactPage() {
     return (
-        <div style={{ padding: '2rem' }}>
-            <h1><Contact></Contact></h1>
+        <div className="contact-container">
+            <h2 className="contact-title">About us</h2>
+            <div className="contact-content">
+                <div className="contact-text">
+                    <p>
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }

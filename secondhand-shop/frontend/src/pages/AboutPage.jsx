7e510315aa77a4b6d7050ac9db3dcbd70b2bcf37
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 11:38:36 +0200

    Add infomation about the shop in about page

--- secondhand-shop/frontend/src/pages/AboutPage.jsx
+++ secondhand-shop/frontend/src/pages/AboutPage.jsx
@@ -1,8 +1,39 @@
+import './CSS/AboutPage.css';
+
 function AboutPage() {
     return (
-        <div style={{ padding: '2rem' }}>
-            <h1>About Shupa Shupa</h1>
-            <p>Secondhand fashion shop.</p>
+        <div className="about-container">
+            <h2 className="about-title">About us</h2>
+            <div className="about-content">
+                <div className="about-image"></div>
+                <div className="about-text">
+                    <p>
+                        Welcome to Shupa Shupa, <br />
+                        <br />A small but passionate secondhand shop built from
+                        my own wardrobe and love for sustainability. I started
+                        Shupa Shupa because I wanted to give my pre-loved
+                        clothes a new life and inspire others to do the same.
+                        Every item in our shop is handpicked, cared for, and
+                        full of stories.
+                    </p>
+                    <p>
+                        By choosing secondhand, you're not just saving
+                        money—you're saving the planet, one garment at a time.
+                        We believe fashion should be slow, stylish, and
+                        sustainable.
+                    </p>
+                    <p>
+                        Our mission is to reduce fast fashion waste and
+                        encourage conscious shopping.
+                    </p>
+                    <p>
+                        Whether you're here to browse, buy, or just get
+                        inspired, I'm so happy you found us. Let's make
+                        secondhand your first choice.
+                    </p>
+                    <p>With love, The Shupa Shupa Team</p>
+                </div>
+            </div>
         </div>
     );
 }

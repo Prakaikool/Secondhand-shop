39e9bdc25669c581aba7a1f46780facb5e0a4599
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 14:17:14 +0200

    Add infomation about the shoop

--- secondhand-shop/frontend/src/components/BottomNavbar.jsx
+++ secondhand-shop/frontend/src/components/BottomNavbar.jsx
@@ -1,12 +1,27 @@
 import '/Secondhand-shop/secondhand-shop/frontend/src/pages/CSS/BottomNavbar.css';
 import { Link } from 'react-router-dom';
 
-function BottomNavbar() {
+const BottomNavbar = () => {
     return (
-        <div style={{ padding: '2rem' }}>
-            <h1>Bottom Navbar</h1>
-        </div>
+        <footer className="bottom-navbar">
+            <div className="bottom-content">
+                <div className="shop-info">
+                    <p>
+                        Shupa Shupa is a small secondhand shop built from love
+                        and sustainability. Every item has a story. Let's make
+                        secondhand your first choice.
+                    </p>
+                    <p>Active everyday 11am–7pm</p>
+                    <p>
+                        Email:{' '}
+                        <a href="mailto:shupashupa@support.com">
+                            shupashupa@support.com
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </footer>
     );
-}
+};
 
 export default BottomNavbar;

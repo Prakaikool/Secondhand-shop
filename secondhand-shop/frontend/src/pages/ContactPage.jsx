07b489e63e1980ee11de4357e67d4e26ff146a1f
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 13:44:35 +0200

    Default

--- secondhand-shop/frontend/src/pages/ContactPage.jsx
+++ secondhand-shop/frontend/src/pages/ContactPage.jsx
@@ -1,10 +1,14 @@
+import './CSS/ContactPage.css';
+
 function ContactPage() {
     return (
         <div className="contact-container">
-            <h2 className="contact-title">About us</h2>
+            <h2 className="contact-title">Contact us</h2>
             <div className="contact-content">
                 <div className="contact-text">
                     <p>
+                        If you have any questions about your order, shipping, or
+                        returns. We're happy to help!
                     </p>
                 </div>
             </div>

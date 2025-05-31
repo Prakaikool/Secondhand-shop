e31ee41b3e8b96745ee229731e76a9f90b1e13ff
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 23:52:40 +0200

    Fetch data from API function

--- secondhand-shop/frontend/src/pages/ShopPage.jsx
+++ secondhand-shop/frontend/src/pages/ShopPage.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
 function ShopPage() {
+    const [products, setProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:5000/products')
+            .then((res) => setProducts(res.data))
+            .catch((err) => console.error('Failed to load products', err));
+
+        axios
+            .get('http://localhost:5000/categories')
+            .then((res) => setCategories(res.data))
+            .catch((err) => console.error('Fail to load categories', err));
+    }, []);
+
     return (
-        <div style={{ padding: '2rem' }}>
-            <h1>Shop</h1>
-        </div>
-    );
+      <div className='shop-page'>
+        <h2>All products</h2>
+
+      </div>
+    )
 }
 
 export default ShopPage;

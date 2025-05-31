0946654b25aa3f4bf5187b9ca781d62c1fe8858e
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-06-01 00:48:03 +0200

    Add sort function (filter)

--- secondhand-shop/frontend/src/pages/ShopPage.jsx
+++ secondhand-shop/frontend/src/pages/ShopPage.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 function ShopPage() {
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('all');
 
     useEffect(() => {
         axios
@@ -19,11 +20,18 @@ function ShopPage() {
     }, []);
 
     return (
-      <div className='shop-page'>
-        <h2>All products</h2>
+        <div className="shop-page">
+            <h2>All products</h2>
+            <div className='filter-container'>
+              <label htmlFor="category">Filter by Category: </label>
+              <select id="category" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                <option value="all">All</option>{categories.map(cat => (<option key={cat.id} value={cat.id}>{cat.name}</option>))}
+              </select>
+            </div>
 
-      </div>
-    )
+      
+        </div>
+    );
 }
 
 export default ShopPage;

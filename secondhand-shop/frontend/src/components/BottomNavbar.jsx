44ff4acdfe89868cfe987564410a02ebad24245d
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 14:14:10 +0200

    Import link and css

--- secondhand-shop/frontend/src/components/BottomNavbar.jsx
+++ secondhand-shop/frontend/src/components/BottomNavbar.jsx
@@ -1,3 +1,6 @@
+import '/Secondhand-shop/secondhand-shop/frontend/src/pages/CSS/BottomNavbar.css';
+import { Link } from 'react-router-dom';
+
 function BottomNavbar() {
     return (
         <div style={{ padding: '2rem' }}>

2b274d6f3ee092e48375df67fc394b9fd212ff92
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-05-31 15:12:50 +0200

    Styling Bottom navbar

--- secondhand-shop/frontend/src/components/BottomNavbar.jsx
+++ secondhand-shop/frontend/src/components/BottomNavbar.jsx
@@ -6,12 +6,13 @@ const BottomNavbar = () => {
         <footer className="bottom-navbar">
             <div className="bottom-content">
                 <div className="shop-info">
+                    <h2>About</h2>
                     <p>
                         Shupa Shupa is a small secondhand shop built from love
                         and sustainability. Every item has a story. Let's make
                         secondhand your first choice.
                     </p>
-                    <p>Active everyday 11am–7pm</p>
+                    <p>Active everyday 11am - 7pm</p>
                     <p>
                         Email:{' '}
                         <a href="mailto:shupashupa@support.com">
@@ -20,21 +21,22 @@ const BottomNavbar = () => {
                     </p>
                 </div>
                 <div className="footer-links">
+                    <h2>Menu</h2>
                     <ul>
                         <li>
-                            <Link to="/shop">Shop</Link>
+                            <Link to="/shop">SHOP</Link>
                         </li>
                         <li>
-                            <Link to="/contact">Contact</Link>
+                            <Link to="/contact">CONTACT</Link>
                         </li>
                         <li>
-                            <Link to="/customer-service">Customer service</Link>
+                            <Link to="/customer-service">CUSTOMER SERVICE</Link>
                         </li>
                         <li>
-                            <Link to="/shipping-policy">Shipping Policy</Link>
+                            <Link to="/shipping-policy">SHIPPING POLICY</Link>
                         </li>
                         <li>
-                            <Link to="/return-policy">Return Policy</Link>
+                            <Link to="/return-policy">RETURN POLICY</Link>
                         </li>
                     </ul>
                 </div>

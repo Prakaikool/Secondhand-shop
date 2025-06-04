8a7ea71c6f735714d4e5ca898649fc765fc2d073
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-06-04 15:30:09 +0200

    Create product create for admin

--- secondhand-shop/frontend/src/admin/ProductCreate.jsx
+++ secondhand-shop/frontend/src/admin/ProductCreate.jsx
@@ -1,1 +1,30 @@
+import * as React from 'react';
+import {
+    Create,
+    SimpleForm,
+    TextInput,
+    NumberInput,
+    ArrayInput,
+    SimpleFormIterator
+} from 'react-admin';
 
+const ProductCreate = (props) => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" fullWidth />
+            <TextInput source="description" multiline fullWidth />
+            <NumberInput source="price" />
+            <TextInput source="size" />
+            <NumberInput source="stock" />
+            <TextInput source="image_url" fullWidth />
+            <ArrayInput source="images">
+                <SimpleFormIterator>
+                    <TextInput />
+                </SimpleFormIterator>
+            </ArrayInput>
+            <NumberInput source="category_id" />
+        </SimpleForm>
+    </Create>
+);
+
+export default ProductCreate;

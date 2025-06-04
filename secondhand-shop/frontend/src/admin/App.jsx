27e0805ee88b50119e10625f2a397d24c8db70df
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-06-04 15:16:30 +0200

    Seting up a basic React Admin panel

---
+++ secondhand-shop/frontend/src/admin/App.jsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Admin, Resource } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import ProductList from './ProductList';
+import ProductEdit from './ProductEdit';
+import ProductCreate from './ProductCreate';
+
+const App = () => (
+    <Admin dataProvider={simpleRestProvider('http://localhost:5000')}>
+        <Resource
+            name="products"
+            list={ProductList}
+            edit={ProductEdit}
+            create={ProductCreate}
+        />
+    </Admin>
+);
+
+export default App;

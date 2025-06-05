216e593e2b57c6918b8ec4456458f8201395b284
Prakaikool Teepraken <prakaikool.teepraken@gmail.com>
2025-06-05 21:24:36 +0200

    Adjust homepage content and styling

--- secondhand-shop/frontend/src/pages/HomePage.jsx
+++ secondhand-shop/frontend/src/pages/HomePage.jsx
@@ -1,12 +1,13 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './CSS/Homepage.css';
 import searchIcon from '../assets/icons/search.png';
 
 function HomePage() {
     const [products, setProducts] = useState([]);
     const [search, setSearch] = useState('');
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
@@ -40,12 +41,14 @@ function HomePage() {
                     <button className="search-button">
                         <img
                             src={searchIcon}
-                            alt="Seach"
+                            alt="Search"
                             className="search-icon"
                         />
                     </button>
                 </div>
             </div>
+
+            {}
             <h2 className="section-title">See what's new</h2>
             <div className="product-grid">
                 {newProducts.map((product) => (
@@ -63,6 +66,20 @@ function HomePage() {
                     </Link>
                 ))}
             </div>
+            <div className='button-shop'>
+                {' '}
+                <button className='more-btn' onClick={() => navigate('/shop')}>View all products</button>
+            </div>
+
+            {}
+            <section className="about-preview">
+                <h2>About Shupa Shupa</h2>
+                <p>
+                    We're a small brand driven by passion for sustainability and
+                    style. Every item is selected with care and intention.
+                </p>
+                <button onClick={() => navigate('/about')}>Learn More</button>
+            </section>
         </div>
     );
 }
